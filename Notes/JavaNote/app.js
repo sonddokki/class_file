f07bb93ac7b431e29f04ce4e5fb85e54d5c15c7e
sonddokki <sonsharo@naver.com>
2023-08-28 11:28:50 +0900

    first commit

---
+++ Notes/JavaNote/app.js
@@ -0,0 +1,38 @@
+// const daysOfweek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// console.log(daysOfweek);
+
+// daysOfweek.push("sun");
+
+// console.log(daysOfweek); 
+
+// function sayHello(nameOfPerson, age) {
+//     console.log("Hello my name is " + nameOfPerson, age);
+// }
+
+// sayHello("nico", 10);
+// sayHello("dal", 23);
+// sayHello("lynn", 21);
+
+
+// function plus(a,b) {
+//     console.log(a + b);
+// }
+
+// plus(8,60);
+
+// function sayHello() {
+
+// }
+
+// const age = 96;
+// function calculateKrAge(ageOfForeigner) {
+//     ageOfForeigner + 2;
+//     return "hello";
+// }
+
+// const krAge = calculateKrAge(age);
+
+// console.log(krAge);
+
+
